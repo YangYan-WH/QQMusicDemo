126990987ae5fa5d63b38e28743040880a0d9fc9
YangYan-WH <39005752+YangYan-WH@users.noreply.github.com>
2018-09-04 12:09:25 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,45 +1,92 @@
 const express = require('express')
 const request = require('request-promise')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 4000
 /*
     npm install express --save
     npm install request --save
     npm install request-promise --save
+    npm install cors --save
 */
 const HEADERS = {
-    'accept':'applicetion/json',
+    'accept': 'application/json',
     'authority': 'c.y.qq.com',
-    'referer': 'https://m.y.qq.com',
     'origin': 'https://m.y.qq.com',
-    'access-control-allow-origin': 'https://192.168.0.167:8080',   
-    // 'Access-Control-Allow-Origin': '*',
-    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1'
+    'referer': 'https://m.y.qq.com/',
+    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
 }
 
-app.get('/',async (req,res)=>{
+async function fetch(url, res) {
+    try {
+        res.json(await request({
+            uri: url,
+            json: true,
+            headers: HEADERS
+        }))
+    } catch (e) {
+        res.json({
+            error: e.message
+        })
+    }
+}
+app.use(cors())
+app.get('/', async (req, res) => {
     const url = `https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&_=${+ new Date()}`
-    try{
-        res.json(await request({
-            uri: url,
-            json: true,
-            headers: HEADERS 
-        }))
-    }catch(e){
-        res.json({error:e.message})
-    }
+    fetch(url, res)
 })
-app.get('/search',async (req,res) => {
-    const {keyword,page=1} = req.query
-    const url=`https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&w=${encodeURIComponent(keyword)}&zhidaqu=1&catZhida=1&t=0&flag=1&ie=utf-8&sem=1&aggr=0&perpage=20&n=20&p=${page}&remoteplace=txt.mqq.all&_=${+new Date()}`
-    try{
-        res.json(await request({
-            uri: url,
-            json: true,
-            headers: HEADERS 
-        }))
-    }catch(e){
-        res.json({error:e.message})
-    }
+
+app.get('/search', async (req, res) => {
+    const {
+        keyword,
+        page = 1
+    } = req.query
+    const url = `https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&w=${encodeURIComponent(keyword)}&zhidaqu=1&catZhida=1&t=0&flag=1&ie=utf-8&sem=1&aggr=0&perpage=20&n=20&p=${page}&remoteplace=txt.mqq.all&_=${+new Date()}`
+    fetch(url, res)
 })
+console.log('我正在伪装服务器')
 app.listen(PORT)
+// const HEADERS = {
+//     'accept':'application/json',
+//     'authority': 'c.y.qq.com',
+//     'referer': 'https://m.y.qq.com',
+//     'origin': 'https://m.y.qq.com',
+//     // 'access-control-allow-origin': 'http://192.168.0.167:8080',
+//     'access-control-allow-origin': 'http://127.0.0.1:8080',
+//     'Access-Control-Allow-Origin': '*',
+//     'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1'
+// }
+// app.use(cors())
+// app.get('/',async (req,res)=>{
+//     const url = `https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&_=${+new Date()}`
+//     try{
+//         res.json(await request({
+//             uri: url,
+//             json: true,
+//             headers: HEADERS 
+//         }))
+//     }catch(e){
+//         console.log('/ :  我运行到错误的地方了')
+//         res.json({error:e.message})
+//     }
+// })
+// app.get('/search', async (req, res) => {
+//     const {
+//         keyword,
+//         page = 1
+//     } = req.query
+//     const url = `https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&w=${encodeURIComponent(keyword)}&zhidaqu=1&catZhida=1&t=0&flag=1&ie=utf-8&sem=1&aggr=0&perpage=20&n=20&p=${page}&remoteplace=txt.mqq.all&_=${+new Date()}`
+//     try {
+//         res.json(await request({
+//             uri: url,
+//             json: true,
+//             headers: HEADERS
+//         }))
+//     } catch (e) {
+//         console.log('/search :  我运行到错误的地方了')
+//         res.json({
+//             error: e.message
+//         })
+//     }
+// })
+// app.listen(PORT)
