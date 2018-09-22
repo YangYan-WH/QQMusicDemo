fafc0dc707cc7fbbc49f5fc3ed1809569723d548
yangyan <15927288912@126.com>
2018-09-22 12:03:40 +0800

    Revert "歌词正则表达式"
    This reverts commit dbe03769b95fb48310f49d7d5f36e1088ab6a8b9.

--- scripts/lyrics_player.js
+++ scripts/lyrics_player.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // class LyricsPlayer {
 //   constructor(el) {
 //     this.$el = el;
@@ -124,9 +125,40 @@ export class LyricsPlayer {
     if (this.index > 2) {
       let y = -(this.index - 2) * this.LINE_HEIGHT
       this.$lines.style.transform = `translateY(${y}px)`
+=======
+class LyricsPlayer {
+    constructor(el) {
+        this.$el = el
+        this.$el.innerHTML = '<div class="player-lyrics-lines"></div>'
+        this.$lines = this.$el.querySelector('.player-lyrics-lines')
+        this.text = ''
+        this.lyrics=[]
+        this.elapsed = 0
+        this.index = 0 
     }
-  }
 
+    start(){
+        this.intervalId = setInterval(this.update.bind(this),50)
+    }
+    pause(){
+        clearInterval(this.intervalId)
+    }
+    render() {
+        let html = this.lyrics.map(line =>`
+            <div class="player-lyrics-line">${line.slice(10)}</div>
+        `).join('')
+        this.$lines.innerHTML = html
+    }
+    update(){
+        this.elapsed +=0.05
+        if(this.elapsed >= this.duration) this.reset()
+        this.progress = this.elapsed / this.duration
+        this.$progress.style.transform = `translate(${this.progress *100 -100}%)`
+        this.$elapsed.innerText = this.formatTime(this.elapsed)
+>>>>>>> parent of dbe0376... 歌词正则表达式
+    }
+
+<<<<<<< HEAD
   render() {
     let html = this.lyrics.map(line => `
       <div class="player-lyrics-line">${line.slice(10)}</div>
@@ -173,3 +205,25 @@ export class LyricsPlayer {
 }
 
 LyricsPlayer.prototype.LINE_HEIGHT = 42
+=======
+    reset(text){
+        this.pause()
+        this.elapsed = 0
+        this.index = 0 
+        if(text){
+            this.text = this.formatText(text) || ''
+            this.lyrics = this.text.match(/^\[\d{2}:\d{2}.\d{2}\](.+)$/gm) || []
+                                           // ^表示行首  $表示行尾  g表示全局  m表示多行 
+            if(this.lyrics.length){
+                this.render()
+                this.$lines.children[this.index].classList.add('active ')
+            }
+        }
+    }
+    formatText (text){
+        let div = document.createElement('div')
+        div.innerHTML = text
+        return div.innerText
+    }
+}
+>>>>>>> parent of dbe0376... 歌词正则表达式
